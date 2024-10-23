c3b3a6f63383a9a81b8621468978ffce94a39f43
Digvijay Singh <digvijay050203@gmail.com>
2024-10-23 16:16:25 +0530

    SSL, RSA implementation

--- client.py
+++ client.py
@@ -1,31 +1,63 @@
 import socket
+import ssl
 import threading
+import base64
+import os
+from dotenv import load_dotenv
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
 
-host = '(server_IP)'
-port = 55556
+# Load environment variables from .env file
+load_dotenv()
 
+# Get paths from env
+public_key_path = os.getenv('PUBLIC_KEY_PATH')
+cert_path = os.getenv('CERT_PATH')
+
+with open(public_key_path, "rb") as key_file:
+    server_public_key = serialization.load_pem_public_key(key_file.read())
+
+host = input("Enter the server IP address: ")
+port = 55545
+
+# Created an SSL context
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
+
+# Wrapped the socket with SSL
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+ssl_client = context.wrap_socket(client, server_hostname=host)
+ssl_client.connect((host, port))
 
-nickname = input("Choose your nickname: ")
+nickname = input("\n\tChoose your nickname: ")
+print("\n")
 
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = ssl_client.recv(1024).decode('ascii')
             if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+                ssl_client.send(nickname.encode('ascii'))
             else:
                 print(message)
         except:
             print("An error occurred!")
-            client.close()
+            ssl_client.close()
             break
 
 def write():
     while True:
         message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
+        encrypted_message = server_public_key.encrypt(
+            message.encode('ascii'),
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        ssl_client.send(base64.b64encode(encrypted_message))
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
